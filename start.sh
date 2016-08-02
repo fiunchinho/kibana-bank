4f1cc13080f15014e47a725bc0527c49e8962f40
Fabri Di Napoli <fabri.dinapoli@gmail.com>
2016-08-02 20:04:15 +0200

    Use docker native instead of docker-machine

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ docker-compose up -d
 
 echo "Waiting Elasticsearch to start..."
 
-while ! nc -z 192.168.99.100 9200; do
+while ! nc -z localhost 9200; do
   sleep 0.1 # wait for 1/10 of the second before check again
 done
 
